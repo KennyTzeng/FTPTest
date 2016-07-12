5a7c5b24f1c42a96d3e087198a7760db6dbd53d4
Kenny Tzeng 曾子軒 <kenny29282000@gmail.com>
2016-07-12 11:03:23 +0800

    Update and rename FTPTest.java to FTPTestDownload.java

--- FTPTestDownload.java
+++ FTPTestDownload.java
@@ -2,20 +2,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 import org.apache.commons.net.ftp.FTPClient;
-//FTP上傳
+//Download File
 public class FTPTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
 			FTPClient ftp = new FTPClient();
-			//連向IP.port
+			//connect to FTP server via IP,port
 			ftp.connect("XXX.XXX.XXX.XXX",21);
-			//登入FTP帳號密碼
+			//login to FTP server by account and password
 			ftp.login("TEST", "TEST");
 			ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+			//specify the file's path and filename which you want to place the file
 			File file = new  File("D:/ftp/interns.xml");
 			FileOutputStream fos = new FileOutputStream(file);
+			//specify the filename your'e going to download at FTP server
 			if(ftp.retrieveFile("interns.xml", fos)){
 				System.out.println("Success");
 			}else{
